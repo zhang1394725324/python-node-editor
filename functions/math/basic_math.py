5023a4b077e16d01bd4caf51b1e4791d16dab6af
Jack.Zhang <53216337+zhang1394725324@users.noreply.github.com>
2026-04-23 17:52:34 +0800

    Update basic_math.py

--- functions/math/basic_math.py
+++ functions/math/basic_math.py
@@ -1,1 +1,15 @@
+def add(a, b):
+    """
+    @name: 加法
+    @description: 返回两个数的和
+    @outputs: sum
+    """
+    return a + b
 
+def multiply(a, b):
+    """
+    @name: 乘法
+    @description: 返回两个数的积
+    @outputs: product
+    """
+    return a * b
